d23a4baad46ae0c13f31f95108bd456bd38a77fd
Thanya <cheveztj@gmail.com>
2025-06-12 10:51:36 -0400

    first commit, just adding assignment to my github

---
+++ app/components/Navbar.js
@@ -0,0 +1,13 @@
+import styles from "../styles/Navbar.module.css"
+
+export default function Navbar(){
+    return (
+        <nav className={styles.nav}>
+            <ul className={styles.ul}>
+                <li>Home</li>
+                <li>Posts</li>
+                <li>Users</li>
+            </ul>
+        </nav>
+    )
+}
